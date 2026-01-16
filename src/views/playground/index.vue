208f515458cebcfb77a165d622603e196a547ee5
Min <solunar@vip.qq.com>
2026-01-16 20:56:17 +0800

    Update

--- src/views/playground/index.vue
+++ src/views/playground/index.vue
@@ -2,14 +2,27 @@
 import * as C from 'cesium';
 import * as T from 'three';
 
+C.Ion.defaultAccessToken = import.meta.env.VITE_ACCESS_TOKEN;
+
 onMounted(() => {
-  console.log(C);
   console.log(T);
 
-  console.log(import.meta.env.VITE_ACCESS_TOKEN);
+  const viewer = new C.Viewer('cesium-container', {
+    terrain: C.Terrain.fromWorldTerrain(),
+  });
+
+  viewer.camera.flyTo({
+    destination: C.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    orientation: {
+      heading: C.Math.toRadians(0.0),
+      pitch: C.Math.toRadians(-15.0),
+    },
+  });
 });
 </script>
 
 <template>
-  <div class="h-screen bg-neutral-200"></div>
+  <div class="h-screen bg-neutral-200">
+    <div class="h-full" id="cesium-container"></div>
+  </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import AutoImport from 'unplugin-auto-import/vite';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
+const cesiumSource = 'node_modules/cesium/Build/Cesium';
+const cesiumBaseUrl = 'cesium-static';
+
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
+  },
   plugins: [
     // https://github.com/unplugin/unplugin-auto-import
     AutoImport({
@@ -21,6 +28,15 @@ export default defineConfig({
 
     // https://tailwindcss.com/docs/installation/using-vite
     tailwindcss(),
+
+    viteStaticCopy({
+      targets: [
+        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
+        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
+      ],
+    }),
 
     vue(),
     vueJsx(),
